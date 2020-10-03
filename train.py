33ba5e6d912027f097d88e21319308956197373e
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-10-03 18:33:31 +0100

    Adds accuracy metric to evaluation. Model performs much worse than it should, probably some detail is wrong.

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from model import VisionTransformer
 
 def select_dataset(task):
     if task == 'mnist':
-        print("> Using MNIST dataset\n")
+        print("> Using MNIST dataset")
         train_dataset = torchvision.datasets.MNIST('data', train=True, download=True,
             transform=torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor()
@@ -70,10 +70,11 @@ def train(model, loader, optim, crit, device):
 
 def evaluate(model, loader, optim, crit, device):
     cumulative_loss = 0.0
+    correct_pred = 0
 
     model.eval()
 
-    for x, y in tqdm.tqdm(loader):
+    for x, y in loader:
         optim.zero_grad()
         x, y = x.to(device), y.to(device)
 
@@ -81,29 +82,33 @@ def evaluate(model, loader, optim, crit, device):
         loss = crit(pred, y)
         cumulative_loss += loss.item()
 
-    return cumulative_loss / len(loader)
+        _, pred = torch.max(pred.data, -1)
+        correct_pred += (pred == y).sum().item()
+
+    return cumulative_loss / len(loader), correct_pred / len(loader)
 
 if __name__ == '__main__':
     TRY_CUDA = True
     DATASET = 'mnist'
-    BATCH_SIZE = 64
+    BATCH_SIZE = 128
     NB_EPOCHS = 100
     ALPHA = 3e-3
     DROPOUT = 0.1
 
     device = torch.device('cuda:0' if TRY_CUDA and torch.cuda.is_available() else 'cpu')
-    print(f"> Device: {device} ({'CUDA is enabled' if TRY_CUDA and torch.cuda.is_available() else 'CUDA not available'}) \n")
+    print(f"> Device: {device} ({'CUDA is enabled' if TRY_CUDA and torch.cuda.is_available() else 'CUDA not available'})")
 
     train_loader, test_loader = select_dataset(DATASET)
     model = create_model(DATASET).to(device)
 
     optim = torch.optim.Adam(model.parameters(), lr=ALPHA)
-    crit = nn.CrossEntropyLoss()
+    crit = nn.NLLLoss()
     
     for ei in range(NB_EPOCHS):
         print(f"> Epoch {ei+1}/{NB_EPOCHS}")
         train_loss = train(model, train_loader, optim, crit, device)
-        eval_loss = evaluate(model, test_loader, optim, crit, device)
+        eval_loss, eval_accuracy = evaluate(model, test_loader, optim, crit, device)
         print(f"> Training Loss: {train_loss}")
-        print(f"> Evaluation Loss: {eval_loss}\n")
+        print(f"> Evaluation Loss: {eval_loss}")
+        print(f"> Evaluation Accuracy: {eval_accuracy:.2f}%\n")
 

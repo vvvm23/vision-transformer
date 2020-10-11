9079d12978a7cab0bfe216c317bb1717d81f0367
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2020-10-11 20:41:32 +0100

    Adds a baseline, resnet model for comparison

--- model.py
+++ model.py
@@ -64,13 +64,54 @@ class VisionTransformer(nn.Module):
         # Decode and output class probabilities
         x = x.view(-1, self.emd_dim*self.nb_patches)
         x = self.decoder(x)
-        x = F.log_softmax(x, dim=-1)
+        x = F.softmax(x, dim=-1)
 
         return x
 
-if __name__ == '__main__':
-    model = VisionTransformer(28, 7, 10, 1, 256, 8, 6, 1024)
-    print(model)
+# Baseline module 
+class ResidualBlock(nn.Module):
+    def __init__(self, nb_channels):
+        super().__init__()
+        self.nb_channels = nb_channels
+        self.conv = nn.Conv2d(nb_channels, nb_channels, kernel_size=3, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(nb_channels)
 
-    x = torch.randn(8, 1, 28, 28)
+        torch.nn.init.kaiming_normal_(self.conv.weight, nonlinearity='relu')
+        torch.nn.init.constant_(self.bn.weight, 0.5)
+        torch.nn.init.zeros_(self.bn.bias)
+
+    def forward(self, x):
+        z = F.relu(self.bn(self.conv(x)))
+        return x + z
+
+class BaseModel(nn.Module):
+    def __init__(self, img_dim, nb_channels, out_dim, res_channels=32, nb_res_blocks=10, mlp_dim=32):
+        super().__init__()
+        self.nb_channels = nb_channels
+        self.img_dim = img_dim
+        self.flatten_dim = res_channels*(img_dim // 4)**2
+
+        self.in_conv = nn.Conv2d(nb_channels, res_channels, kernel_size=3, padding=1)
+        self.res_layers = nn.Sequential(*(nb_res_blocks*[ResidualBlock(res_channels)]))
+        self.mlp1 = nn.Linear(self.flatten_dim, mlp_dim)
+        self.mlp2 = nn.Linear(mlp_dim, out_dim)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.in_conv(x)), 2)
+        x = self.res_layers(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, self.flatten_dim)
+        x = F.relu(self.mlp1(x))
+        x = self.mlp2(x)
+        return F.softmax(x, dim=-1)
+
+if __name__ == '__main__':
+    # model = VisionTransformer(28, 7, 10, 1, 256, 8, 6, 1024)
+    # print(model)
+
+    # x = torch.randn(8, 1, 28, 28)
+    # print(model(x))
+    model = BaseModel(32, 3, 10)
+    x = torch.randn(1, 3, 32, 32)
+    print(model)
     print(model(x))
